19614ffcb8f39060443b44d10b30722880328810
jailmao <jai.robinson@outlook.com>
2024-03-19 22:21:52 +0000

    cond dist update

--- Project.R
+++ Project.R
@@ -9,17 +9,18 @@ library(ADGofTest)
 library(tseries)
 library(fBasics)
 library(MASS)
-
+#import the historical data
 n225 <- read.csv("~/STAT/ICA/N225.csv", stringsAsFactors=FALSE)
 fchi <- read.csv("~/STAT/ICA/FCHI.csv", stringsAsFactors=FALSE)
 
+#creates a function returning log returns
 n225_r <- function(t) {
   return (log(as.numeric(n225[t+1,6]))-log(as.numeric(n225[t,6])))
 }
 fchi_r <- function(t) {
   return (log(as.numeric(fchi[t+1,6]))-log(as.numeric(fchi[t,6])))
 }
-
+#construct a data set with adjclose values as well as log returns
 numOfWeeks = nrow(n225)
 returnsDataSet <- data.frame(matrix(ncol=5, nrow=0, dimnames=list(NULL, c("Week", "n225_returns", "fchi_returns", "n225_adjclose", "fchi_adjclose"))))
 
@@ -56,14 +57,41 @@ pacf(returnsDataSet$fchi_returns, col="green", lwd=2)
 acf(returnsDataSet$fchi_returns^2, col="red", lwd=2)
 pacf(returnsDataSet$fchi_returns^2, col="red", lwd=2)
 #so we will use 3 lags. (?)
+#which conditional distribution should we use for the garch model?
+distributions<- c("norm", "std", "sstd", "snorm", "sged", "ged")
+cond_dist_table1 <- data.frame(matrix(ncol=3, nrow=0, dimnames=list(NULL, c("dist", "aic", "bic"))))
 
-modeln225=garchFit(formula=~arma(3,0)+garch(1,1),data=returnsDataSet$n225_returns,trace=F,cond.dist="norm")
-modelfchi=garchFit(formula=~arma(3,0)+garch(1,1),data=returnsDataSet$fchi_returns,trace=F,cond.dist="norm")
-modeln225
+for (k in 1:6){  
+  model1=garchFit(formula=~arma(3,0)+garch(1,1),data=returnsDataSet$n225_returns,trace=F,cond.dist=distributions[k])
+  cond_dist_table1[k,1]=distributions[k]
+  
+  cond_dist_table1[k,2]=(summary(model1))$ics["AIC"]
+  cond_dist_table1[k,3]=(summary(model1))$ics["BIC"]
+  }
+
+cond_dist_table1
+#so we use the sstd
+cond_dist_table2 <- data.frame(matrix(ncol=3, nrow=0, dimnames=list(NULL, c("dist", "aic", "bic"))))
+for (k in 1:6){  
+  model1=garchFit(formula=~arma(3,0)+garch(1,1),data=returnsDataSet$fchi_returns,trace=F,cond.dist=distributions[k])
+  cond_dist_table2[k,1]=distributions[k]
+  
+  cond_dist_table2[k,2]=(summary(model1))$ics["AIC"]
+  cond_dist_table2[k,3]=(summary(model1))$ics["BIC"]
+}
+
+cond_dist_table2
+#we choose also sstd
+#we conclude by fitting an ar(3)garch(1,1) model to both data
+model1=garchFit(formula=~arma(3,0)+garch(1,1),data=returnsDataSet$n225_returns,trace=F,cond.dist="sstd")
+model2=garchFit(formula=~arma(3,0)+garch(1,1),data=returnsDataSet$fchi_returns,trace=F,cond.dist="sstd")
+
+
+
 
 
 # Step 3: Model checking
-res1 <- residuals(modeln225, standardize=TRUE)
+res1 <- residuals(model1, standardize=TRUE)
 par(mfrow=c(2,1))
 acf(res1, col="green", lwd=2)
 acf(res1^2, col="red", lwd=2)
@@ -73,7 +101,8 @@ Box.test(res1^2, lag = 10, type = c("Ljung-Box"), fitdf = 1)
 u1<-pnorm(res1, mean=0, sd=1)[4:numOfWeeks-3]
 hist(u1)
 print (rev(u1))
-res2 <- residuals(modelfchi, standardize=TRUE)
+
+res2 <- residuals(model2, standardize=TRUE)
 par(mfrow=c(2,1))
 acf(res2, col="green", lwd=2)
 acf(res2^2, col="red", lwd=2)
@@ -90,9 +119,12 @@ hist(u2)
 #generating epsilon using copulas
 copModel=BiCopSelect(u1, u2, familyset=NA, selectioncrit="AIC", indeptest=TRUE, level=0.05,se = TRUE)
 copModel
+copModel2=BiCopSelect(u1, u2, familyset=NA, selectioncrit="BIC", indeptest=TRUE, level=0.05,se = TRUE)
+copModel2
+
 #the best model to use is the Bivariate copula: t (par = 0.6, par2 = 17.31, tau = 0.41) so we will use students t
 N=10000
-varEstimate1 <- slot(modeln225, "sigma.t")
+varEstimate1 <- slot(model1, "sigma.t")
 varEstimate1
 u_sim=BiCopSim(N, family=copModel$family, copModel$par,  copModel$par2)
 res1_sim=qnorm(u_sim[,1], mean = 0, sd = 1) 
@@ -100,29 +132,29 @@ res2_sim=qnorm(u_sim[,2], mean = 0, sd = 1)
 res1_sim
 print(tail(u1, n=1))
 #introduce GARCH effects
-sigmaSquaredHat1=modeln225@fit$coef["omega"] + modeln225@fit$coef["alpha1"] * (tail(u1, n=1))^2+ modeln225@fit$coef["beta1"] * (tail(varEstimate1, n=1))^2
+sigmaSquaredHat1=model1@fit$coef["omega"] + model1@fit$coef["alpha1"] * (tail(u1, n=1))^2+ model1@fit$coef["beta1"] * (tail(varEstimate1, n=1))^2
 tail=as.numeric(tail(returnsDataSet$n225_returns, n=3))
 
-sim_values_n225 = (modeln225@fit$coef["mu"]+modeln225@fit$coef["ar1"]*tail[3] +modeln225@fit$coef["ar2"]*tail[2] +modeln225@fit$coef["ar3"]*tail[1])+sigmaSquaredHat1*res1_sim
+sim_values1 = (model1@fit$coef["mu"]+model1@fit$coef["ar1"]*tail[3] +model1@fit$coef["ar2"]*tail[2] +model1@fit$coef["ar3"]*tail[1])+sigmaSquaredHat1*res1_sim
 
 
 #and for the other model
-varEstimate2 <- slot(modelfchi, "sigma.t")
+varEstimate2 <- slot(model2, "sigma.t")
 varEstimate2
-sigmaSquaredHat2=modelfchi@fit$coef["omega"] + modelfchi@fit$coef["alpha1"] * (tail(u2, n=1))^2+ modelfchi@fit$coef["beta1"] * (tail(varEstimate2, n=1))^2
+sigmaSquaredHat2=model2@fit$coef["omega"] + model2@fit$coef["alpha1"] * (tail(u2, n=1))^2+ model2@fit$coef["beta1"] * (tail(varEstimate2, n=1))^2
 tail2=as.numeric(tail(returnsDataSet$fchi_returns, n=3))
 
-sim_values_fchi = (modelfchi@fit$coef["mu"]+modelfchi@fit$coef["ar1"]*tail2[3] +modelfchi@fit$coef["ar2"]*tail2[2] +modelfchi@fit$coef["ar3"]*tail2[1])+sigmaSquaredHat2*res2_sim
-sim_values_n225
-sim_values_fchi
+sim_values_2 = (model2@fit$coef["mu"]+model2@fit$coef["ar1"]*tail2[3] +model2@fit$coef["ar2"]*tail2[2] +model2@fit$coef["ar3"]*tail2[1])+sigmaSquaredHat2*res2_sim
+sim_values1
+sim_values_2
 #find var
-hist(sim_values_n225)
+hist(sim_values1)
 #this looks like a normal distribution  so lets fit one
 # Fit normal distribution to log-transformed values
 
 
-fit1 <- fitdistr(sim_values_n225, densfun = "normal")
-fit2 <- fitdistr(sim_values_fchi, densfun = "normal")
+fit1 <- fitdistr(sim_values1, densfun = "normal")
+fit2 <- fitdistr(sim_values_2, densfun = "normal")
 portfolio_mean=0.5*(fit1$estimate[1]+fit2$estimate[1])
 portfolio_sd=sqrt(0.25*((fit1$estimate[2])^2+(fit2$estimate[2])^2)+0.5*(fit1$estimate[2])*(fit2$estimate[2])*cor(returnsDataSet$n225_returns, returnsDataSet$fchi_returns))
 
